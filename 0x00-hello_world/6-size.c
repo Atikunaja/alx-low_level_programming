14eb572d3a99abbb36544d3740d1b82b56117117
Atikunaja <najaatiku1@gmail.com>
2023-08-10 19:35:19 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
 
 int main(void)
-
 {
 
 	printf("Size of a char: %c byte(s)\n", sizeof(char));
@@ -21,4 +21,3 @@ int main(void)
 	return (0);
 
 }
-
