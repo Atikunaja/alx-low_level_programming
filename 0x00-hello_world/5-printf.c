8fb5909ec22f3297152973d96fb7fc962641d43e
Atikunaja <najaatiku1@gmail.com>
2023-08-10 19:06:24 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
-* main  program that prints the outcome of a string
+* main - program that prints the outcome of a string
 * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	char string[] = "\"with proper grammar, but the outcome is a piece of art";
+
+	printf(string);
 	return (0);
 }
