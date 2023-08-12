6d269a5371c49d85baa10bb48385d7d4a00218bd
Atikunaja <najaatiku1@gmail.com>
2023-08-12 02:37:35 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -22,10 +22,7 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-	
 		printf("%d is negative\n", n);
 	}
-	else
-
 		return (0);
 }
