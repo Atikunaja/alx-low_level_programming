739142e2ee908a5f1d39c1fb503c31a6c813f38f
Atikunaja <najaatiku1@gmail.com>
2023-08-16 14:27:25 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main  - A program that prints alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+void print_alphabet(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(%c);
+		putchar('\n');
+	}	
+		return(0);
+}
