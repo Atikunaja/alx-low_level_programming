e4a8f6a51afddc8da7357d9ce38c6d3c28888edb
Atikunaja <najaatiku1@gmail.com>
2023-08-20 10:20:24 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,11 @@
 
 int main(void)
 {
-	int  digit;
-
-	while (digit <= 10)
-	{
-		putchar(digit);
-		digit++;
-	}
-		putchar ('\n');
-
-		return (0);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
