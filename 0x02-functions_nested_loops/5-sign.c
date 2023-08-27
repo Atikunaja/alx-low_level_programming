e48ddded9f37a1b4cfe0cc05788b3caf6c121b9a
Atikunaja <najaatiku1@gmail.com>
2023-08-27 02:30:43 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_sign - print sign of a number
  * @n: function parameter
  * Return: 1, 0, or -1
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -17,6 +19,8 @@ int print_sign(int n)
 		return (0);
 	}
 	else
+	{	
 	_putchar('-');
 	return (-1);
+	}
 }
