9b8e2011a147a90fb84b884924fdced070df2c65
Atikunaja <najaatiku1@gmail.com>
2023-09-01 12:38:06 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - function that multiplies two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: a * b
+ */
+int _isdigit(int c)
+{
+        return (a * b);
+}
