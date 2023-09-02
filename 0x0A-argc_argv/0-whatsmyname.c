d72c75a4afb3c7db30bf147e39c33a642c9a775d
Atikunaja <najaatiku1@gmail.com>
2023-09-02 05:51:25 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: argument vector
+ * main: function name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
