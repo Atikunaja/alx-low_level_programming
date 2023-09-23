01754896f533375f27c17529c184892609c69226
Atikunaja <najaatiku1@gmail.com>
2023-09-23 08:50:52 -0700

    a sentence

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 void first(void) __attribute__((constructor));
 /**
- * first _ prints the first sentence
+ * first _ prints a sentence before the main
+ * function is executed
  */
 void first(void)
 {
-	printf("you're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back! \n");
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
