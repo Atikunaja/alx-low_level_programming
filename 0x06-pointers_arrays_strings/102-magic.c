8a2832cb2bdc6b6226752d28618879661c6024a4
Atikunaja <najaatiku1@gmail.com>
2023-09-26 16:45:05 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,16 +6,15 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &n
-
+	p = &n;
 /**
- *
- *
- *
- *
+ * main - Entry point
+ * @P: pointer
+ * @a: array
+ * @n: parameter
+ * Return: void
  */
-	(*p + s) = 98;
-	printf("a[2] = %d\n", a[2])
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
-
