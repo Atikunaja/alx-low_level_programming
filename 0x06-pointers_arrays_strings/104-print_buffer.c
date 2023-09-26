0eb2cff38d28332a1aa8068ae33d2f1eb9b2de7f
Atikunaja <najaatiku1@gmail.com>
2023-09-26 16:19:43 -0700

    infinite add

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,18 +16,20 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-	while (o < size)
+	while (o < 10)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
+		o += 10;
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 			printf("%02x", *(b + o + i));
-		}
-		printf("  ");
+		else
+			printf("  ");
 		if (i % 2)
-				printf(" ");
+			printf(" ");
+	}
 	for (i = 0; i < j; i++)
 	{
 		int c = *(b + o + i);
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -15,5 +16,4 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
 #endif
