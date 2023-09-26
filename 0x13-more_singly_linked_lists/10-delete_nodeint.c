30c62794da215144806443f35067970339a71dba
Atikunaja <najaatiku1@gmail.com>
2023-09-26 02:28:27 -0700

    free list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,10 +8,10 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-
 	listint_t *current = NULL;
 	unsigned int i = 0;
-	if (*head == NULL) 
+
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
@@ -24,12 +24,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 	if (!temp || !(temp->next))
 		return (-1);
+	}
 	temp = temp->next;
 		i++;
-	}
 	current = temp->next;
 	temp->next = current->next;
 	free(current);
-	
-	return (1);
+		return (1);
 }
