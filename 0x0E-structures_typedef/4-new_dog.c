963ae80def1a594102673fbe84ae4aed87ac7eee
Atikunaja <najaatiku1@gmail.com>
2023-09-26 03:48:20 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,7 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
