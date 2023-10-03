eddc01304f45c5d37157d10dfd350a6caed25f0f
Atikunaja <najaatiku1@gmail.com>
2023-10-02 20:50:01 -0700

    close

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,27 +15,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
+	if (fd == -1){
 		return (0);
-	}
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
-	{
 		close(fd);
 		return (-1);
-	}
 	t = read(fd, buf, letters);
 	if(t == -1)
-	{
-		free(buf);
 		close(fd);
 		return (-1);
-	}
 	w = write(STDOUT_FILENO, buf, t);
 	if (w == -1)
 	{
-		free(buf);
 		close(fd);
 		return (-1);
 	}
