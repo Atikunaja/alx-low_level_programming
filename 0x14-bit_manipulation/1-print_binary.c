9e17bbca574db7de0e9963cf578a6bdb20a56563
Atikunaja <najaatiku1@gmail.com>
2023-12-14 13:19:13 -0800

    binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,17 +5,17 @@
  */
 void print_binary(unsigned long int n)
 {
-	int a, m = 0;
+	int leona, Sis_hoffmann = 0;
 	unsigned long int new;
-
-	for (a = 63; a >= 0; a--)
+	
+	for (leona = 63; leona >= 0; leona--)
 	{
-		new = m >> a;
+		new = n >> leona;
 		if (new & 1)
 		{
 			_putchar('1');
-			m++;
-		}
+			Sis_hoffmann++;
+                }
 		else if (new)
 			_putchar('0');
 	}
